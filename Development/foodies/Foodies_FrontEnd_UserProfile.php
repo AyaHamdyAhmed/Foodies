ca8c4ab9651a48ab351417007dcbf5baccd363a4
MariamNesiem <34419548+MariamNesiem@users.noreply.github.com>
2019-05-24 22:32:27 +0200

    Update Foodies_FrontEnd_UserProfile.php

--- Development/foodies/Foodies_FrontEnd_UserProfile.php
+++ Development/foodies/Foodies_FrontEnd_UserProfile.php
@@ -49,12 +49,12 @@ if (isset($_SESSION['UserID']) && $_SESSION['IdUserType']==2) {
 <center><img src="Photos/foodies.png" alt="user photo" style="width:200px; height:100px;"></center>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="main.css">
+    <!-- <link rel="stylesheet" href="main.css">-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>-->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <style>
         body {
             background-image: url("Photos/food.jpg");
